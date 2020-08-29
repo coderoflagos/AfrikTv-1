56f6d7a00290d59a39bf4ee3139b5ccec5d60626
Zubs <zubairidrisaweda@gmail.com>
2020-08-29 19:58:31 +0100

    Sorted all auth stuff

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -91,6 +91,7 @@
       <link rel="stylesheet" href="{{ asset('template\bootstrap-4.4.1\dist\css\bootstrap.min.css') }}">
       <!-- Custom stylesheet - for your changes-->
       <link rel="stylesheet" href="{{ asset('template/register.css') }}">
+      <link rel="stylesheet" href="{{ asset('css/register.css') }}">
       <!-- Tweaks for older IEs-->
       <!--[if lt IE 9]>
           <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -109,15 +110,28 @@
 <div class="col-sm-6" style="margin-top: 10%;">
     <h1>Create an Account</h1>    
 
-      <form>
+      <form method="POST" action="{{ route('register') }}">
+        @csrf
     <div class="form-group row">
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="name" placeholder="Username" style="border-radius: 30px;">
+            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus style="border-radius: 30px;" placeholder="{{ __('Name') }}">
+
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
  <div class="form-group row">
         <div class="col-sm-10">
-            <input type="text" class="form-control" id="name" placeholder="Email Address" style="border-radius: 30px;">
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" style="border-radius: 30px;" placeholder="{{ __('E-Mail Address') }}">
+
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
 
@@ -129,14 +143,20 @@
 
 
     <div class="form-group row">
-        <div class="col-sm-10"> 
-            <input type="password" class="form-control" id="inputPassword" placeholder="Password" style="border-radius: 30px; width: 100%;">
+        <div class="col-sm-10">
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" style="border-radius: 30px; width: 100%;" placeholder="{{ __('Password') }}">
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
 
     <div class="form-group row">
         <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword" placeholder="Confirm Password" style="border-radius: 30px; width: 100%;">
+            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" style="border-radius: 30px; width: 100%;" placeholder="{{ __('Confirm Password') }}">
         </div>
     </div>
 <br>
@@ -147,7 +167,7 @@
 
     <div class="form-group">
    <br>
-            <button type="submit" class="btn btn-primary pull-right" style="border-radius: 20px; background-color: maroon; width: 82%; border-color: maroon;">Sign Up</button>
+    <button type="submit" class="btn btn-primary pull-right" style="border-radius: 20px; background-color: maroon; width: 82%; border-color: maroon;">{{ __('Sign Up') }}</button>
         </div>
     </div>
 </form>
@@ -277,76 +297,6 @@
 </footer>
 <!-- Footer -->
   </div>
-
-<style type="text/css">
-  .dark-mode {
-  background-color: black;
-  color: white;
-}
-
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-
-/* Hide default HTML checkbox */
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* The slider */
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
-
-input:checked + .slider {
-  background-color: #2196F3;
-}
-
-input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
-}
-
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
-
-.slider.round:before {
-  border-radius: 50%;
-}
-
-</style>
 
   <script>
 function myFunction() {
